0e01d43f329da846edea7f72f63a60274bec1477
Khuong Nguyen <nguyenthecoder@gmail.com>
2021-05-23 18:09:44 -0400

    First push

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View 
+      style={{
+        width: '100%',
+        height: '100%',
+        flexDirection: "row",  
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor:'gray'
+      }}
+    >
+{/* 
+      <Text>Open up App.js to start working on your app! Somethine here hahahah</Text>
       <StatusBar style="auto" />
+      <Button title="Here"/> */}
+
+      <View 
+        style={{
+          backgroundColor: "red",
+          width:100,
+          height:100,
+          alignItems: 'center',
+          justifyContent:'center'
+          }}>
+            <Text>1</Text>
+          </View>
+
+      <View 
+        style={{
+          backgroundColor: "blue",
+          justifyContent: "center",
+          alignItems: "center",
+          width:100,
+          height:100,
+          }}>
+            <Text>1</Text>
+          </View>
+
+      <View 
+        style={{
+          backgroundColor: "green",
+          justifyContent: 'center',
+          alignItems: 'center',
+          width:100,
+          height:100,
+          }}>
+            <Text>1</Text>
+      </View>
+
     </View>
   );
 }
